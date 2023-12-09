1441c37e717292b737978e00dfb35269c8ea9d31
Walter N <2760walter@gmail.com>
2023-12-09 14:03:42 +0300

    compelte

---
+++ lib/migrations/versions/15d3583d2fb8_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 15d3583d2fb8
+Revises: 
+Create Date: 2023-12-09 12:28:54.882818
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '15d3583d2fb8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
